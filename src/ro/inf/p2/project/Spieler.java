e7d6cf94d919375bd4ce5a087ce483c86d58e472
DKauth <Dominik.Kauth@gmx.de>
2014-06-17 09:11:58 +0200

    Spieler.java

---
+++ src/ro/inf/p2/project/Spieler.java
@@ -0,0 +1,45 @@
+package ro.inf.p2.project;
+
+import java.util.ArrayList;
+
+public class Spieler implements ISpieler  {
+
+	public boolean entferneSpielFigur(int posX, int posY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean entferneSpielFigur(ISpielFigur figur) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void fuegeDameZurListeHinzu(ISpielFigur figur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setzeName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int gibAnzahlSteine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ArrayList<ISpielFigur> gibFiguren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String gibName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+
+}
